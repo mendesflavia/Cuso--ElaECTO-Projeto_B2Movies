af29880e18e8f490fb07dc97bba48b68feb521f1
mendesflavia <mendesflavia@hotmail.com>
2021-02-12 18:26:58 -0300

    arrumanod routes

--- back-end/src/routes/filmesRoutes.js
+++ back-end/src/routes/filmesRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require("../controller/animesController");
+const controller = require("../controller/filmesController");
 
 router.get("/", controller.listarTodosfilmes);
 router.get("/:id", controller.pesquisaPorId);
